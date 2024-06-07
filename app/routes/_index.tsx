5d30b4933faf9374fc641a252b4cbb984e6bd09e
Mehul Behera <mbehera@umd.edu>
2024-06-07 11:10:49 -0400

    added daisyui

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
+import Navbar from "./navbar";
 
 export const meta: MetaFunction = () => {
   return [
@@ -10,7 +11,7 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div>
-      <h1 className="">Hello</h1>
+      <Navbar />
     </div>
   );
 }
---
+++ app/routes/navbar.tsx
@@ -0,0 +1,20 @@
+function Navbar() {
+  return (
+    <div className="navbar bg-base-100">
+      <div className="navbar-start"></div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <a href="surveylistpage">Survey List</a>
+          </li>
+          <li>
+            <a href="createsurveypage">Create Survey</a>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-end"></div>
+    </div>
+  );
+}
+
+export default Navbar;
